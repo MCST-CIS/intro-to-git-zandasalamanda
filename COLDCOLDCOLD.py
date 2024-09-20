649481cfdf694f8ee75292f2cdee973dd0a689fe
zandasalamanda <159577543+zandasalamanda@users.noreply.github.com>
2024-09-20 09:17:32 -0400

    Add files via upload

---
+++ COLDCOLDCOLD.py
@@ -0,0 +1,95 @@
+totalscore = 0
+
+#Question 1:
+
+print("Question 1: What is the name of Maxwell Brohm's TikTok account?")
+
+print("A) Brawl Stars - Maximalicious42")
+
+print("B) Maxxy_DaGoat49")
+
+print("C) Brawl Stars - MaxMillion404")
+
+print("D) MaxineGotThatWaxine2009")
+
+answer1 = input ('Answer: ')
+
+if answer1 == 'C':
+    print('Correct!')
+    totalscore += 1
+
+#Question 2:
+
+print("Question 2: How many followers does Maxwell Brohm's account have on TikTok?")
+
+print("A) 1,524")
+
+print("B) 6")
+
+print("C) 637")
+
+print("D) 58")
+
+answer2 = input ('Answer: ')
+
+if answer2 == 'A':
+    print('Correct!')
+    totalscore += 1
+
+#Question 3:
+
+print("Question 3: What is the name of Maxwell Brohm's dog?")
+
+print("A) Zhōnghuá Rénmín Gònghéguó")
+
+print("B) Noah Blum")
+
+print("C) Cristiano Ronaldo dos Santos Aveiro")
+
+print("D) Pearl")
+
+answer3 = input ('Answer: ')
+
+if answer3 == 'D':
+    print('Correct!')
+    totalscore += 1
+
+
+
+#Question 4:
+
+print("Question 4: Who is Maxwell Brohm's wife?")
+
+print("A) Stratton Jelley")
+
+print("B) Noah Blum")
+
+print("C) Zander Leon")
+
+print("D) All of the above")
+
+answer4 = input ('Answer: ')
+
+if answer4 == 'B':
+    print('Correct!')
+    totalscore += 1
+
+
+
+#Question 5:
+
+print("Question 5: What is Maxwell Brohm's favorite artist?")
+
+print("A) 22 Pilots")
+
+print("B) 73 Conductors")
+
+print("C) 28 Sailors")
+
+print("D) 21 Pilots")
+
+answer5 = input ('Answer: ')
+
+if answer5 == 'D':
+    print('Correct!')
+    totalscore += 1
